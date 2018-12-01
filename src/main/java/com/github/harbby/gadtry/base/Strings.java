82026092290bf1b580bac41e87217e819fd32148
ideal <yezhixinghai@gmail.com>
2018-12-01 16:02:14 +0800

    checkstyle

--- src/main/java/com/github/harbby/gadtry/base/Strings.java
+++ src/main/java/com/github/harbby/gadtry/base/Strings.java
@@ -26,10 +26,14 @@ public class Strings
 
     public static boolean isBlank(final CharSequence cs)
     {
-        int strLen;
-        if (cs == null || (strLen = cs.length()) == 0) {
+        if (cs == null) {
             return true;
         }
+        int strLen = cs.length();
+        if (strLen == 0) {
+            return true;
+        }
+
         for (int i = 0; i < strLen; i++) {
             if (Character.isWhitespace(cs.charAt(i)) == false) {
                 return false;
