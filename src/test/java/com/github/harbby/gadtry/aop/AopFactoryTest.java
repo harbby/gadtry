2f5fe498d76ab424e953fe2cf86606c7f4049ce2
ideal <yezhixinghai@gmail.com>
2019-04-23 20:36:04 +0800

    remove proxyInstance()

--- src/test/java/com/github/harbby/gadtry/aop/AopFactoryTest.java
+++ src/test/java/com/github/harbby/gadtry/aop/AopFactoryTest.java
@@ -18,9 +18,7 @@ package com.github.harbby.gadtry.aop;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class AopFactoryTest
@@ -47,17 +45,6 @@ public class AopFactoryTest
 
         Set set = aopFactory.proxy(Set.class, new HashSet());
         set.clear();
-    }
-
-    @Test
-    public void proxyInstanceTest()
-    {
-        Map<String, Integer> map = AopFactory.proxyInstance(new HashMap<String, Integer>())
-                .byClass(Map.class)
-                .after(methodInfo -> {
-                    System.out.println(methodInfo);
-                });
-        Assert.assertEquals(map, new HashMap<>());
     }
 
     private static <T> T getProxy(Class<T> key, T instance)
