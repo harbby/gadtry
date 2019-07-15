93dbac2b373c15dfe83d9866e8116a6cf8727353
yezhixinghai@gamil.com <hepeng1991>
2019-07-15 23:39:22 +0800

    v1.6 support mock

--- src/test/java/com/github/harbby/gadtry/aop/MockTest.java
+++ src/test/java/com/github/harbby/gadtry/aop/MockTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.harbby.gadtry.ioc;
+package com.github.harbby.gadtry.aop;
 
 import com.github.harbby.gadtry.aop.mock.MockGo;
 import com.github.harbby.gadtry.base.Throwables;
