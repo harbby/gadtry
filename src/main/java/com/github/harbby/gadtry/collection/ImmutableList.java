b686f38c44002faf061e09b92bdbd1814dfd7fd4
harbby <yezhixinghai@gmail.com>
2020-12-23 13:40:43 +0800

    fix ImmutableList serialize

--- src/main/java/com/github/harbby/gadtry/collection/ImmutableList.java
+++ src/main/java/com/github/harbby/gadtry/collection/ImmutableList.java
@@ -16,8 +16,6 @@
 
 package com.github.harbby.gadtry.collection;
 
-import com.github.harbby.gadtry.base.Iterators;
-
 import java.io.Serializable;
 import java.util.AbstractList;
 import java.util.Collection;
@@ -26,6 +24,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 import java.util.RandomAccess;
 
 import static java.util.Objects.requireNonNull;
@@ -34,29 +33,30 @@ public abstract class ImmutableList<E>
         extends AbstractList<E>
         implements List<E>
 {
-    private static final List<?> EMPTY = new ImmutableArrayList<>(new Object[0]);
-
+    @SuppressWarnings("unchecked")
     public static <T> List<T> copy(Iterable<? extends T> iterable)
     {
         if (iterable instanceof Collection) {
-            return new ImmutableArrayList<>((((Collection<? extends T>) iterable).toArray()));
+            return new ImmutableArrayList<>((T[]) ((Collection<? extends T>) iterable).toArray());
         }
         else {
-            return new ImmutableArrayList<>(MutableList.copy(iterable).toArray()); //todo: 2 copy 引用
+            return new ImmutableArrayList<>((T[]) MutableList.copy(iterable).toArray());
         }
     }
 
+    @SuppressWarnings("unchecked")
     public static <T> ImmutableList<T> copy(Iterator<? extends T> iterator)
     {
-        return new ImmutableArrayList<>(MutableList.copy(iterator).toArray()); //todo: 2 copy 引用
+        return new ImmutableArrayList<>((T[]) MutableList.copy(iterator).toArray());
     }
 
+    @SuppressWarnings("unchecked")
     @SafeVarargs
     public static <T> List<T> of(T... elements)
     {
         switch (elements.length) {
             case 0:
-                return (List<T>) ImmutableList.EMPTY;
+                return (List<T>) ImmutableArrayList.EMPTY;
             case 1:
                 return Collections.singletonList(elements[0]);
             default:
@@ -66,59 +66,67 @@ public abstract class ImmutableList<E>
 
     public static <T> List<T> of(T t1)
     {
-        return new ImmutableArrayList<>(new Object[] {t1});
+        return checkedArr(t1);
     }
 
     public static <T> List<T> of(T t1, T t2)
     {
-        return new ImmutableArrayList<>(new Object[] {t1, t2});
+        return checkedArr(t1, t2);
     }
 
     public static <T> List<T> of(T t1, T t2, T t3)
     {
-        return new ImmutableArrayList<>(new Object[] {t1, t2, t3});
+        return checkedArr(t1, t2, t3);
     }
 
     public static <T> List<T> of(T t1, T t2, T t3, T t4)
     {
-        return new ImmutableArrayList<>(new Object[] {t1, t2, t3, t4});
+        return checkedArr(t1, t2, t3, t4);
     }
 
     public static <T> List<T> of(T t1, T t2, T t3, T t4, T t5)
     {
-        return new ImmutableArrayList<>(new Object[] {t1, t2, t3, t4, t5});
+        return checkedArr(t1, t2, t3, t4, t5);
     }
 
     public static <T> List<T> of(T t1, T t2, T t3, T t4, T t5, T t6)
     {
-        return new ImmutableArrayList<>(new Object[] {t1, t2, t3, t4, t5, t6});
+        return checkedArr(t1, t2, t3, t4, t5, t6);
     }
 
     public static <T> List<T> of(T t1, T t2, T t3, T t4, T t5, T t6, T t7)
     {
-        return new ImmutableArrayList<>(new Object[] {t1, t2, t3, t4, t5, t6, t7});
+        return checkedArr(t1, t2, t3, t4, t5, t6, t7);
     }
 
     public static <T> List<T> of(T t1, T t2, T t3, T t4, T t5, T t6, T t7, T t8)
     {
-        return new ImmutableArrayList<>(new Object[] {t1, t2, t3, t4, t5, t6, t7, t8});
+        return checkedArr(t1, t2, t3, t4, t5, t6, t7, t8);
     }
 
-    static class ImmutableArrayList<E>
+    @SafeVarargs
+    private static <T> ImmutableArrayList<T> checkedArr(T... array)
+    {
+        return new ImmutableArrayList<>(requireNonNull(array, "array is null"));
+    }
+
+    private static class ImmutableArrayList<E>
             extends ImmutableList<E>
             implements RandomAccess, Serializable
     {
-        private final Object[] array;
+        private static final ImmutableArrayList<Object> EMPTY = new ImmutableArrayList<>(new Object[0]);
 
-        public ImmutableArrayList(Object[] array)
+        private final E[] array;
+
+        private ImmutableArrayList(E[] array)
         {
-            this.array = requireNonNull(array, "array is null");
+            this.array = array;
         }
 
         @Override
         public E get(int index)
         {
-            return (E) array[index];
+            return array[index];
         }
 
         @Override
@@ -130,17 +138,94 @@ public abstract class ImmutableList<E>
         @Override
         public Iterator<E> iterator()
         {
-            return (Iterator<E>) Iterators.of(array);
+            return listIterator();
         }
 
         @Override
         public ListIterator<E> listIterator(int index)
         {
-            throw new UnsupportedOperationException();
+            return new ImmutableListIterator<>(array, index);
         }
 
         @Override
         public void sort(Comparator<? super E> c)
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private static class ImmutableListIterator<E>
+            implements ListIterator<E>
+    {
+        private final E[] array;
+        private int position;
+
+        private ImmutableListIterator(E[] array, int index)
+        {
+            this.array = array;
+            this.position = index;
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return position < array.length;
+        }
+
+        @Override
+        public E next()
+        {
+            if (position < array.length) {
+                return array[position++];
+            }
+            else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public boolean hasPrevious()
+        {
+            return position > 0;
+        }
+
+        @Override
+        public E previous()
+        {
+            if (position > -1) {
+                return array[--position];
+            }
+            else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public int nextIndex()
+        {
+            return position;
+        }
+
+        @Override
+        public int previousIndex()
+        {
+            return position - 1;
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void set(E e)
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void add(E e)
         {
             throw new UnsupportedOperationException();
         }
